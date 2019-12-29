5ee5e35e93553373d865ab57cf2969a0fe9fbafe
yulc <350009335@qq.com>
2019-12-29 11:13:45 +0800

    SSL方式发送邮件

--- mail_notice.py
+++ mail_notice.py
@@ -11,15 +11,16 @@ SEND_PWD = "5794f5eddda2da1e"
 RECEIVE = "mr.yulichao@foxmail.com"
 
 
-def send_mail(title, content, mail_host='smtp.sina.com', port=25):
+def send_mail(title, content, mail_host='smtp.sina.com', ssl_port=465):
     """
-    发送邮件函数，默认使用smtp.sina.com
+    发送邮件函数，阿里云默认禁用25端口，所以用SSL方式发送
     :param title: 邮件标题
     :param content: 邮件内容
     :param mail_host: 邮箱服务器
-    :param port: 端口号
+    :param ssl_port: SSL端口号
     :return:
     """
+    print("开始发送<<<<<<")
     # 邮件内容
     msg = MIMEText(content)
     # 邮件主题
@@ -28,10 +29,11 @@ def send_mail(title, content, mail_host='smtp.sina.com', port=25):
     msg['From'] = SEND_USER
     # 接收者账号列表
     msg['To'] = RECEIVE
-    # 连接邮箱，传入邮箱地址，和端口号，smtp的端口号是25
-    smtp = smtplib.SMTP(mail_host, port=port)
+    # 连接邮箱，传入邮箱地址，和端口号
+    smtp = smtplib.SMTP_SSL(mail_host, port=ssl_port)
     # 发送者的邮箱账号，密码
     smtp.login(SEND_USER, SEND_PWD)
     # 参数分别是发送者，接收者，第三个是把上面的发送邮件的内容变成字符串
     smtp.sendmail(SEND_USER, RECEIVE, msg.as_string())
     smtp.quit()  # 发送完毕后退出smtp
+    print("发送完毕>>>>>>>")
